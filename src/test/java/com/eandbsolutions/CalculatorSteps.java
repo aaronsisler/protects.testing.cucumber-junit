74cf3eff5be1adc6b675fb2d8d037e4aef1a3866
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2024-08-03 12:04:18 -0400

    I got the two flavors of things working

--- src/test/java/com/eandbsolutions/CalculatorSteps.java
+++ src/test/java/com/eandbsolutions/CalculatorSteps.java
@@ -1,21 +1,42 @@
 package com.eandbsolutions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class CalculatorSteps {
-    @Given("I have two numbers")
-    public void iHaveTwoNumbers() {
 
+    private int firstValue;
+    private int secondValue;
+    private int calculatedValue;
+
+    @Given("I have {int} and {int}")
+    public void iHaveAnd(int arg0, int arg1) {
+        firstValue = arg0;
+        secondValue = arg1;
     }
 
     @When("I add those numbers")
     public void iAddThoseNumbers() {
-
+        calculatedValue = new Calculator().add(firstValue, secondValue);
     }
 
+    @Then("I get {int}")
+    public void iGet(int arg0) {
+        Assertions.assertEquals(calculatedValue, arg0);
+    }
+
+    @Given("I have two numbers")
+    public void iHaveTwoNumbers(DataTable dataTable) {
+        firstValue = Integer.parseInt(dataTable.column(0).get(0));
+        secondValue = Integer.parseInt(dataTable.column(1).get(0));
+    }
+
+
     @Then("I get the correct result")
-    public void iGetTheCorrectResult() {
+    public void iGetTheCorrectResult(DataTable dataTable) {
+        Assertions.assertEquals(Integer.parseInt(dataTable.row(0).get(0)), calculatedValue);
     }
 }
--- src/test/resources/com/eandbsolutions/Calculator.feature
+++ src/test/resources/com/eandbsolutions/Calculator.feature
@@ -1,6 +1,19 @@
 Feature: Calculator
 
   Scenario: Add some integers
+    Given I have 1 and 4
+    When I add those numbers
+    Then I get 5
+
+
+  Scenario Outline: Add some integers from table
     Given I have two numbers
+      | <first> | <second> |
     When I add those numbers
     Then I get the correct result
+      | <result> |
+
+    Examples:
+      | first | second | result |
+      | 2     | 5      | 7      |
+      | 3     | 7      | 10     |
